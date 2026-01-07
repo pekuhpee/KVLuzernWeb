2ceb3cd05513032cbc99be39a3880859f4f748f9
pqp <83675377+pekuhpee@users.noreply.github.com>
2026-01-07 22:00:46 +0100

    Add public content item upload

---
+++ apps/exams/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from apps.exams import views
+
+app_name = "exams"
+
+urlpatterns = [
+    path("upload", views.upload, name="upload"),
+    path("upload/danke", views.upload_thanks, name="upload_thanks"),
+]
